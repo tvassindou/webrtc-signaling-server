11d455545a7637161a6aecda41f84c3c3c5dfaed
tvassindou <vassindou.toure@orange.com>
2022-09-25 12:32:50 +0000

    add favicon

--- src/app.ts
+++ src/app.ts
@@ -16,18 +16,11 @@ export class RenderStreaming {
             if (Array.isArray(argv)) {
                 program
                     .usage('[options] <apps...>')
-                    .option('-s, --secure', 'Enable HTTPS (you need server.key and server.cert)', process.env.SECURE || false)
-                    .option('-k, --keyfile <path>', 'https key file (default server.key)', process.env.KEYFILE || 'server.key')
-                    .option('-c, --certfile <path>', 'https cert file (default server.cert)', process.env.CERTFILE || 'server.cert')
                     .option('-l, --logging <type>', 'Choose http logging type combined, dev, short, tiny or none.(default dev)', process.env.LOGGING || 'dev')
                     .parse(argv);
                 const option = program.opts();
                 return {
                     port: normalizePort(process.env.PORT || '80'),
-                    secure: option.secure == undefined ? false : option.secure,
-                    keyfile: option.keyfile,
-                    certfile: option.certfile,
-                    websocket: true,
                     mode: 'public',
                     logging: option.logging,
                 };
@@ -46,29 +39,26 @@ export class RenderStreaming {
     constructor(options: Options) {
         this.options = options;
         this.app = createServer(this.options);
-        if (this.options.secure) {
-            this.server = https.createServer({
-                key: fs.readFileSync(options.keyfile),
-                cert: fs.readFileSync(options.certfile),
-            }, this.app).listen(this.options.port, () => {
-                const { port } = this.server.address() as AddressInfo;
-                const addresses = this.getIPAddress();
-                for (const address of addresses) {
+        this.server = this.app.listen(this.options.port, () => {
+            const { port } = this.server.address() as AddressInfo;
+            const addresses = this.getIPAddress();
+            for (const address of addresses) {
+
+                if (this.options.port == 443) {
                     console.log(`https://${address}:${port}`);
-                }
-            });
-        } else {
-            this.server = this.app.listen(this.options.port, () => {
-                const { port } = this.server.address() as AddressInfo;
-                const addresses = this.getIPAddress();
-                for (const address of addresses) {
+
+                } else {
                     console.log(`http://${address}:${port}`);
                 }
-            });
-        }
+            }
+        });
 
         if (this.options.websocket) {
-            console.log(`start websocket signaling server ws://${this.getIPAddress()[0]}`);
+            var prefix: string = "ws";
+            if (this.options.port == 443) {
+                prefix = "wss";
+            }
+            console.log(`start websocket signaling server ${prefix}://${this.getIPAddress()[0]}`);
             //Start Websocket Signaling server
             new WSSignaling(this.server, this.options.mode);
         }
@@ -89,7 +79,7 @@ export class RenderStreaming {
         }
         return addresses;
     }
-    
+
 }
 function normalizePort(val) {
     var port = parseInt(val, 10);
